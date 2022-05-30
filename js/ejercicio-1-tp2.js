694e0e440eb30aa086729cd898f79308045d22c7
osvaldo rodriguez <osvaldotuc64@gmail.com>
2022-05-30 11:35:49 -0300

    tp-1

--- js/ejercicio-1-tp2.js
+++ js/ejercicio-1-tp2.js
@@ -1,1 +1,9 @@
 /*1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.*/
+
+let edad = parseInt(prompt('ingrese su edad'));
+
+if(edad >= 18){
+    document.write('Ya puedes conducir!!');
+}else if(edad <18){
+    document.write('Eres menor de 18 años, no puedes conducir!!');
+}
