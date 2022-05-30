ec797284b12cf0f204ea43ac2f9dbcde569f105a
osvaldo rodriguez <osvaldotuc64@gmail.com>
2022-05-30 12:15:16 -0300

    tp-2

--- js/ejercicio-2-tp2.js
+++ js/ejercicio-2-tp2.js
@@ -8,3 +8,36 @@
 10: Sobresaliente
 
 Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”. */
+
+let numero = parseInt(prompt('ingresar su calificacion de 0 a 10'));
+
+if (numero === 0 || numero === 1 || numero === 2) {
+    alert('Muy deficiente');
+    }
+
+if (numero === 3 || numero === 4) {
+     alert('Insuficiente');
+    }
+if (numero === 5 || numero === 6) {
+    alert('Suficiente');
+    }        
+
+if (numero === 5 || numero === 6) {
+    alert('Suficiente');
+    }
+
+if (numero === 7) {
+     alert('Bien');
+     }
+
+if (numero === 8 || numero === 9) {
+     alert('Notable');
+    }     
+
+if (numero === 10) {
+     alert('Sobresaliente');
+     }
+
+ if(numero >11){
+     alert('Introduce un numero valido')
+ }
