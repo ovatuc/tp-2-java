c6c164d6ca14f2f1ebe73ae837a21da687c676a5
osvaldo rodriguez <osvaldotuc64@gmail.com>
2022-05-31 10:57:22 -0300

    e-3-4-5

--- js/ejercicio-3-tp2.js
+++ js/ejercicio-3-tp2.js
@@ -1,3 +1,21 @@
 /*3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
 
 Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp*/
+
+
+let concatenar;
+let texto = prompt("Escriba una cadena de texto para salir selecione cancelar");
+
+for (let i = 0, j = 0; texto != null; i++) {
+  if (i == 0) {
+    concatenar = texto;
+  } else {
+    texto = prompt("Escriba una cadena de texto para salir selecione cancelar");
+    if (texto != null) {
+      concatenar = concatenar + "-" + texto;
+    }
+  }
+}
+
+document.write(concatenar);
+
--- js/ejercicio-5-tp2.js
+++ js/ejercicio-5-tp2.js
@@ -7,3 +7,24 @@ Debemos calcular el resto de la división entera entre el número y el número 2
 Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E) 
 Si lo introducido no es un número deberá indicarse con un alert y volver a preguntar.
 Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».*/
+
+let dni = null;
+let letra = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+do {
+    dni = prompt("Ingrese su DNI");
+    if (dni!=null)
+    {
+        dni=parseInt(dni);
+        if (isNaN(dni))
+            alert("ingrese un valor númerico");
+        else if (dni<0 || dni>99999999)
+            alert("Ingrese un valor entre 0 y 99999999");
+        else{
+            let index = dni % 23;
+            alert(dni + ' corresponde a la letra ' + letra [index])
+        }
+    }
+ 
+} while (dni!=null)
+
